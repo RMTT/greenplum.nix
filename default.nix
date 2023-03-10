8c267e93b48ad595dfdf61d77d9519ea24745cfc
RMT <d.rong@outlook.com>
2023-03-10 23:06:09 +0800

    add simple development environment

---
+++ default.nix
@@ -0,0 +1,5 @@
+{ }:
+
+{
+    devenv = import ./environment {}
+}
---
+++ environments/default.nix
@@ -0,0 +1,45 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  hello_message = "Welcome to use and develop Greenplum Database";
+
+  devTools = with pkgs; [
+    ripgrep
+    gitui
+    htop
+    cowsay
+    zsh
+    git
+    gh
+    neovim
+    man
+    man-pages
+    pipenv
+    gdb
+  ];
+
+  devDependencies = with pkgs; [
+    readline
+    zlib
+    zstd
+    apr
+    libevent
+    libxml2
+    bzip2
+    curl
+    libxcrypt
+    python3Full
+  ];
+
+  shell = pkgs.mkShell.override {stdenv = pkgs.gcc12Stdenv;};
+in
+  shell {
+    buildInputs = devTools ++ devDependencies;
+    shellHook = ''
+      cowsay -e ^^ ${hello_message}
+    '';
+
+    LC_ALL = "en_US.UTF-8";
+    LANGUAGE = "en_US.UTF-8";
+    LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+  }
