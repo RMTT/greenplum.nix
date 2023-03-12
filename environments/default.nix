364cec07d0ea2aa22361e06c8816b58ead0f4633
RMT <d.rong@outlook.com>
2023-03-13 00:09:02 +0800

    update environments name

--- environments/default.nix
+++ environments/default.nix
@@ -2,9 +2,11 @@
 {
     gpenv-6X = pkgs.callPackage ./greenplum-env.nix {
         greenplumDrv = pkgs.greenplum-db-6X_STABLE;
+        name = "gpenv-6X";
     };
 
     gpenv-main = pkgs.callPackage ./greenplum-env.nix {
         greenplumDrv = pkgs.greenplum-db-main;
+        name = "gpenv-main";
     };
 }
