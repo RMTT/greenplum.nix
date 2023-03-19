2945c276dbb0a147bdf198be26ad43b6ebf3f392
RMT <d.rong@outlook.com>
2023-03-19 23:53:24 +0800

    update readme and version number

--- README.md
+++ README.md
@@ -8,33 +8,27 @@ Install [Nix](https://nixos.org/)
 ### Install Greenplum Package
 
 ```shell
-nix-channel --add "https://github.com/RMTT/greenplum.nix/archive/main.tar.gz" greenplum
-nix-channel --update
+nix registry add greenplum github:RMTT/greenplum.nix
 ```
 
 Now, you can install some gpdb versions which have been defined in this repo:
 ```shell
-nix-env -qa "greenplum.*"
+nix search greenplum
 ```
 
 To install greenplum db:
 ```
-nix-env -i {greenplum version}
+nix profile install {greenplum version}
 ```
 
 To start shell with specific greenplum version:
 ```
-nix-shell -p {greeplum version}
+nix shell greenplum#{greenplum version}
 ```
 
 ### Use Greenplum development envrionment
 
-List all avaible environment:
-```shell
-nix-env -qa "gpenv.*"
-```
-
 Enter environment:
 ```shell
-nix-shell "<greenplum>" -A {gpenv version}
+nix develop greenplum#{greenplum version}
 ```
--- packages/default.nix
+++ packages/default.nix
@@ -2,28 +2,28 @@
 
 let
     repo_pkgs = rec {
-        default = greenplum-db-main;
+        default = main;
 
-        greenplum-db-6-23-3 = pkgs.callPackage ./greenplum6.nix {
+        "6.23.3" = pkgs.callPackage ./greenplum6.nix {
             stdenv = pkgs.gcc8Stdenv;
             makeFlags = ["-j8"];
             version = "6.23.3";
             rev = "0eb759d759987e82ba3bf910b89ed3057bad0416";
         };
 
-        greenplum-db-6X_STABLE = pkgs.callPackage ./greenplum6.nix {
+        "6X_STABLE" = pkgs.callPackage ./greenplum6.nix {
             stdenv = pkgs.gcc8Stdenv;
             makeFlags = ["-j8"];
             rev = "5ee1530829413dd820d82178e289f7cc8c7be363";
         };
 
-        greenplum-db-main = pkgs.callPackage ./greenplum7.nix {
+        main = pkgs.callPackage ./greenplum7.nix {
             stdenv = pkgs.gcc12Stdenv;
             makeFlags = ["-j8"];
             rev = "18710643c4a00bfa99c2ed4f54c19c4d03b5394e";
         };
 
-        greenplum-db-7-0-0-beta-1 = pkgs.callPackage ./greenplum7.nix {
+        "7.0.0.beta-1" = pkgs.callPackage ./greenplum7.nix {
             stdenv = pkgs.gcc12Stdenv;
             makeFlags = ["-j8"];
             version = "7.0.0-beta.1";
