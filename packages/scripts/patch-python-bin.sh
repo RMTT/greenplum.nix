9214aa087493319c9e94a69eb0bc9865489b21ca
RMT <d.rong@outlook.com>
2023-03-26 15:59:47 +0800

    replace python3 binary

--- packages/scripts/patch-python-bin.sh
+++ packages/scripts/patch-python-bin.sh
@@ -12,3 +12,15 @@ for f in ${files[@]}; do
     echo "find 'python -c' in ${f}, will be replaced"
     sed -i "s%python -c%${newPythonBin} -c%g" "$f"
 done
+
+
+newPythonBin=$(command -v python3)
+echo "replace 'python3 -c' to '${newPythonBin} -c'"
+
+# files contain "python -c"
+files=$(rg "python3 -c" -l $out)
+
+for f in ${files[@]}; do
+    echo "find 'python3 -c' in ${f}, will be replaced"
+    sed -i "s%python3 -c%${newPythonBin} -c%g" "$f"
+done
