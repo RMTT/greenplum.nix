96a5d86fb4ab2ddf1c30df11d958252df6b2d32e
RMT <d.rong@outlook.com>
2023-03-28 14:59:32 +0800

    using stable drv for environments

--- environments/default.nix
+++ environments/default.nix
@@ -3,12 +3,12 @@ rec {
     default = gpenv-main;
 
     gpenv-6X = pkgs.callPackage ./greenplum-env.nix {
-        greenplumDrv = pkgs.greenplum-db-6X_STABLE;
+        greenplumDrv = pkgs.greenplum-db-6-23-4;
         name = "gpenv-6X";
     };
 
     gpenv-main = pkgs.callPackage ./greenplum-env.nix {
-        greenplumDrv = pkgs.greenplum-db-main;
+        greenplumDrv = pkgs.greenplum-db-7-0-0-beta-1;
         name = "gpenv-main";
     };
 }
