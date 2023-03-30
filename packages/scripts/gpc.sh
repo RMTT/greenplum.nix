a87e34d45a2cefcd5cfe6572a99926f5e10b2127
RMT <d.rong@outlook.com>
2023-03-30 09:42:59 +0800

    update gpc

--- packages/scripts/gpc.sh
+++ packages/scripts/gpc.sh
@@ -1,5 +1,15 @@
 #!/bin/env bash
 
+usage() {
+    echo "usage: gpc {original greenplum command and args}"
+    echo "for example: gpc gpstart -a"
+}
+
+if [ "$#" -eq "0" ]; then
+    usage
+    exit 1
+fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 GPHOME=$(realpath "$SCRIPT_DIR"/..)
 
