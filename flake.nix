0c2d4a36483ee18bb4e93ed1a4e0aaabe8b9b6c2
RMT <d.rong@outlook.com>
2023-04-04 14:14:21 +0800

    remove hydra jobs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
                 {
                     packages = packages;
                     devShells = environments;
-                    hydraJobs.build = packages;
                 }
             );
 }
